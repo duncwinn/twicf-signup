8244ce507de5823869653aa320372a75559e9385
Duncan Winn <dwinn@pivotal.io>
2014-12-17 11:15:32 +0000

    Added bootstrap UI for provfiles and added gif for CF image
    Added additional boostrap components for UI on the admin server.

--- src/main/java/io/pivotal/twicf/signup/AdminController.java
+++ src/main/java/io/pivotal/twicf/signup/AdminController.java
@@ -1,6 +1,7 @@
 package io.pivotal.twicf.signup;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.Cloud;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +22,9 @@ final class AdminController {
     private final Environment environment;
     private final SubscriptionRepository subscriptionRepository;
 
+    @Autowired(required = false)
+    private Cloud cloud;
+
     @Autowired
     AdminController(Environment environment, SubscriptionRepository subscriptionRepository) {
         this.environment = environment;
@@ -30,7 +34,12 @@ final class AdminController {
     @RequestMapping(method = RequestMethod.GET, value = "/admin")
     String displayForm(Model model) {
         model.addAttribute("profiles", this.environment.getActiveProfiles());
-//        model.addAttribute("services", this.environment.getActiveProfiles());
+
+        if (cloud != null) {
+            System.out.println("Cloud services:" + cloud.getServiceInfos());
+            model.addAttribute("services", cloud.getServiceInfos());
+        }
+
         return "adminForm";
     }
 
